5c2e76db4e3924d6ee8b88dd618ef9908fedf1b7
Jox <joxched@gmail.com>
2016-01-13 16:19:14 -0800

    add tutorial basic 2 functions

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array();
+$autoload['libraries'] = array('database', 'parser');
 
 /*
 | -------------------------------------------------------------------
@@ -127,3 +127,6 @@ $autoload['language'] = array();
 |	$autoload['model'] = array('first_model' => 'first');
 */
 $autoload['model'] = array();
+
+
+$autoload['helper'] = array('common', 'url');
