88cd8256f58a428b12b9299040111195a0764f58
Jessica <jessicasaini@hotmail.com>
2016-01-20 10:10:20 -0800

    fixing references

--- application/views/_cell.php
+++ application/views/_cell.php
@@ -7,7 +7,7 @@
  */
 
 ?>
-<a href ="./data/{filename}" data-lightbox="gallery"
+<a href ="/data/{filename}" data-lightbox="gallery"
    data-title="{title} ... Post ed {uploaded} by {uploader}, in {category}">
-    <img src="./data/thumb/{filename}"/>
+    <img src="/data/thumb/{filename}"/>
 </a>
